a280e681b5c6039a284549e6b8f5e7dd16a7374c
Julieon1 <rjpeterson@coe.edu>
2019-12-04 17:31:11 -0600

    HTML Functionality

--- ServerPublisher/SignHTTPServer.py
+++ ServerPublisher/SignHTTPServer.py
@@ -4,11 +4,12 @@
 
 from netTools import *
 from Sign_Publisher import *
+from bs4 import BeautifulSoup
 import threading
 import time
 import random
 
-
+signList = {}
 
 def processRequest(s, addr):
 
@@ -50,6 +51,8 @@ def processRequest(s, addr):
             response += inFile.read()
             s.send(response.encode("ascii"))
 
+
+##################################################
     elif HTTPmsg[:4] == "POST":
 
         HTTPmsg = HTTPmsg[5:]
@@ -57,7 +60,83 @@ def processRequest(s, addr):
         resource = HTTPmsg[:spaceLoc]
         resource = resource.strip("/")
 
-        if resource[-3:].lower() == "php":
+
+##################################################
+        if resource[-4:].lower() == "html":
+            dataLoc = HTTPmsg.split("\n")
+            inputData = dataLoc[-1]
+            response = "HTTP/1.0 200 OK\n"
+            response += "Content-Type: text/html\n"
+            response +="\n" 
+
+            formList = []
+            current = ""
+            for i in range(len(inputData)):
+                if inputData[i-1] == "=":
+                    current = ""
+                if inputData[i] == "&":
+                    formList.append(current)
+                    print(current)
+                    current = ""
+                    #response += "\n"
+                else:
+                    current += inputData[i]
+                    #response += inputData[i]
+            formList.append(current)
+
+            if formList[0] == "signage":
+                html = open("SignageTemplate.html").read()
+                soup = BeautifulSoup(html)
+                    
+
+                newLine = []
+                newLine = soup.new_tag('br')
+
+                soup.body.append(newLine)
+                for i in range(len(formList)):
+                    newLine[i] = soup.new_tag('br')
+                    newLine[i].string = formList[i]
+                    soup.body.append(newLine[i])
+
+                open("Signage.html", "w").write(str(soup))
+
+                inFile = open(resource, "r")
+                response += inFile.read()
+                s.send(response.encode("ascii"))
+
+
+
+            else:
+                #######Return page with data to user########
+                html = open("AddedTemplate.html").read()
+                soup = BeautifulSoup(html)
+                    
+
+                newLine = []
+                newLine = soup.new_tag('br')
+
+                soup.body.append(newLine)
+                for i in range(len(formList)):
+                    newLine[i] = soup.new_tag('br')
+                    newLine[i].string = formList[i]
+                    soup.body.append(newLine[i])
+
+                open("Added.html", "w").write(str(soup))
+
+                inFile = open(resource, "r")
+                response += inFile.read()
+                s.send(response.encode("ascii"))
+                
+
+                makeSign(inputData)
+
+                #key = 0
+                #while signList[key] != null:
+                #    key+=1
+                #signList[key] = formList
+
+#################################################################
+        elif resource[-3:].lower() == "php":
             dataLoc = HTTPmsg.split("\n")
             inputData = dataLoc[-1]
             response = "HTTP/1.0 200 OK\n"
@@ -86,6 +165,12 @@ def processRequest(s, addr):
             s.send(response.encode("ascii"))
 
             makeSign(inputData)
+
+            key = 0
+            while signList[key]:
+                key+=1
+            signList[key] = formList
+                
 ##            if formList[0] == "event":
 ##                makeEvent(formList[2], formList[3], formList[1], "Test")
 ##            f.write ("\n")
